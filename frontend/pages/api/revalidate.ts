0ff42eca2b0052efd646de882ccca325ae68e5ff
Vadim Ilchenko <v.ilchenko@mate.academy>
2022-05-17 22:31:47 +0300

    use on-demand revalidation

---
+++ frontend/pages/api/revalidate.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  try {
+    await res.unstable_revalidate('/');
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send('Error revalidating');
+  }
+}
