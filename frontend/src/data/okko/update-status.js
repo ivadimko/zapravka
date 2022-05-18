0585ebf4eaf03cf0cb11a77ce233b158453a20ed
Vadim Ilchenko <v.ilchenko@mate.academy>
2022-05-18 13:48:01 +0300

    update okko stations

--- frontend/src/data/okko/update-status.js
+++ frontend/src/data/okko/update-status.js
@@ -26,7 +26,7 @@ const parseSchedule = (content) => (content.split(SCHEDULE).pop() || '').trim();
   const response = await fetch('https://www.okko.ua/api/uk/fuel-map', {
     headers: {
       accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'accept-language': 'uk,en-US;q=0.9,en;q=0.8,la;q=0.7',
+      'accept-language': 'uk',
       'cache-control': 'no-cache',
       pragma: 'no-cache',
       'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
@@ -37,7 +37,7 @@ const parseSchedule = (content) => (content.split(SCHEDULE).pop() || '').trim();
       'sec-fetch-site': 'none',
       'sec-fetch-user': '?1',
       'upgrade-insecure-requests': '1',
-      cookie: 'visid_incap_2141272=oAdB/9RjQhe9PP2EzYH3UHCqhGIAAAAAQUIPAAAAAAAlgKJ1BdfsQat//WyDCZMh; incap_ses_1510_2141272=oxglG2bymh8Joj0WT5n0FHCqhGIAAAAApa3d/aRijVUdmdToX+SeaA==; incap_ses_324_2141272=8Nr3DOVbTgEgDFoKmhR/BGWrhGIAAAAAOdvgxn9x93cLRbKu4/JExw==; _gcl_au=1.1.537078840.1652861806; _ga=GA1.2.385587420.1652861806; _gid=GA1.2.1401782244.1652861806; homeBanerSlide=2',
+      cookie: 'visid_incap_2141272=hnr24SnWSpu+aI4qVFoMOprOhGIAAAAAQUIPAAAAAAC6MSNwgrJNIOMeAxnZynAE; incap_ses_1104_2141272=PLeeBQso+GxCEixBzjJSD5rOhGIAAAAAkopA3mCBS01MLn4yZ4MPpQ==',
     },
     referrerPolicy: 'strict-origin-when-cross-origin',
     body: null,
