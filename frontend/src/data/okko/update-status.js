f5960cf02f1badbc8f2a178f6fd984d2427ac694
Vadim Ilchenko <v.ilchenko@mate.academy>
2022-05-24 19:30:01 +0300

    fix okko stations parser

--- frontend/src/data/okko/update-status.js
+++ frontend/src/data/okko/update-status.js
@@ -3,7 +3,7 @@ const parser = require('node-html-parser');
 const { fetchOkkoData } = require('./okko-fetcher');
 
 const SCHEDULE = 'Графік роботи:';
-const AVAILABLE_CASH = 'За готівку і банківські картки доступно:';
+const AVAILABLE_CASH = 'За готівку і банківські картки доступно';
 const AVAILABLE_FUEL_CARDS = 'З паливною карткою і талонами доступно:';
 
 const parseFuel = (options) => {
