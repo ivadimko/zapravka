a354d0ec6e2f780bf5607d5d00283a1617229cc4
Vadim Ilchenko <v.ilchenko@mate.academy>
2022-05-24 19:29:33 +0300

    fix okko stations parser

--- frontend/src/controllers/providers/okko/okko.fetcher.ts
+++ frontend/src/controllers/providers/okko/okko.fetcher.ts
@@ -20,7 +20,7 @@ interface AllStationsApiResponse {
 }
 
 const SCHEDULE = 'Графік роботи:';
-const AVAILABLE_CASH = 'За готівку і банківські картки доступно:';
+const AVAILABLE_CASH = 'За готівку і банківські картки доступно';
 const AVAILABLE_FUEL_CARDS = 'З паливною карткою і талонами доступно:';
 
 const parseFuel = (options: {
