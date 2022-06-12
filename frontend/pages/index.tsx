3d60e5bad4a5a6ed6e54d66809160f657931f977
Vadim Ilchenko <v.ilchenko@mate.academy>
2022-06-12 13:07:53 +0300

    fix page title

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -32,7 +32,7 @@ const Home: NextPage<Props> = (props) => {
     }),
   }), [revalidated]);
 
-  const title = `Пальне в наявності на ${updatedAt.short}`;
+  const title = 'Пальне в наявності сьогодні';
   const description = 'Де заправитись? Інтерактивна мапа наявності пального на АЗК в Україні. Статус заправок WOG, OKKO, SOCAR, UPG, АВІАС ПЛЮС, АВІАС, ANP, Sentosa Oіl, МАВЕКС, УКРТАТНАФТА, ЮКОН, ЭЛИН, ЮКОН Сервіс, Rubіx, МАВЕКС плюс, Петрол Гарант, ЗНП, БРСМ Нафта.';
 
   return (
