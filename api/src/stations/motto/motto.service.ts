8f586f3fb2c6e3351a60568154c06ea4d211e0c1
Vadim Ilchenko <v.ilchenko@mate.academy>
2022-07-04 15:41:02 +0300

    remove for scrape from motto

--- api/src/stations/motto/motto.service.ts
+++ api/src/stations/motto/motto.service.ts
@@ -12,7 +12,6 @@ export class MottoService {
   private readonly scraper = new MottoScraper();
 
   async findAll(attempt = 1) {
-    await this.scrape();
     try {
       const file = await fs.readFile(this.filePath);
 
